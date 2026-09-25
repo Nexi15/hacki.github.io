7edfe2e5eb82e805a19cfcb7c66ba8be07602b73
Nexi15 <puchatek524@gmail.com>
2026-09-25 03:45:44 +0200

    Refactor Firebase setup to use SDK imports
    Updated Firebase configuration to use SDK imports and initialize Firebase app.

--- organizacje/script.js
+++ organizacje/script.js
@@ -1,14 +1,21 @@
-// Konfiguracja Twojej bazy Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyARd3zgC_7SnLZG4c1rmx1E7TOK4sqy2zQ",
-    authDomain: "lostmc-db.firebaseapp.com",
-    databaseURL: "https://lostmc-db-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "lostmc-db",
-    storageBucket: "lostmc-db.firebasestorage.app",
-    messagingSenderId: "400562944733",
-    appId: "1:400562944733:web:f33720ac90878966557934",
-    measurementId: "G-MLMBYZXXFF"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARd3zgC_7SnLZG4c1rmx1E7TOK4sqy2zQ",
+  authDomain: "lostmc-db.firebaseapp.com",
+  databaseURL: "https://lostmc-db-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lostmc-db",
+  storageBucket: "lostmc-db.firebasestorage.app",
+  messagingSenderId: "400562944733",
+  appId: "1:400562944733:web:f33720ac90878966557934",
+  measurementId: "G-MLMBYZXXFF"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Połączenie z bazą
 if (!firebase.apps.length) {
